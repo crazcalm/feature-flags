8d316e9f8bd705171d42229036177d48e66fa127
Marcus Willock <crazcalm@gmail.com>
2021-11-27 00:36:26 -0500

    Fix Clippy issues and prototype writing tests for cli subcommand

--- src/bin/cli/subcommands/delete_flags.rs
+++ src/bin/cli/subcommands/delete_flags.rs
@@ -1,7 +1,7 @@
 use feature_flags::db::{delete_flag_by_name, DBLocal};
 
 pub fn delete_flag(db: DBLocal, name: String) {
-    let result = delete_flag_by_name(db.clone(), name);
+    let result = delete_flag_by_name(db, name);
     match result {
         Ok(deleted) => {
             println!("{} row deleted", deleted);
